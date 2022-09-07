64c21a3851971cd396cc97882f6e9f679060c983
wujian <wujian@skieer.com>
2022-09-07 18:47:06 +0800

    init

---
+++ src/template/normal.js
@@ -0,0 +1,33 @@
+/**
+ * @fileOverview
+ *
+ * 往右布局结构模板
+ *
+ * @author: wxs
+ * @copyright:  2022
+ */
+define(function (require, exports, module) {
+  var template = require("../core/template");
+
+  template.register("normal", {
+    getLayout: function (node) {
+      var level = node.getLevel();
+      //console.log(node.data, "cc");
+
+      // 根节点
+      if (level === 0) {
+        return "right";
+      }
+      // // 一级节点
+      // if (level === 1) {
+      //   return "right";
+      // }
+      return "right";
+    },
+
+    getConnect: function (node) {
+      return "poly-circle";
+      // return "poly";
+    },
+  });
+});
---
+++ src/theme/normal.js
@@ -0,0 +1,69 @@
+define(function (require, exports, module) {
+  var theme = require("../core/theme");
+  var commonFontColor = "#262626";
+  var commonBkColor = "#f1f1f4";
+  var commonFontSize = 14;
+  var commonColor = "#7262FD";
+
+  // 连接线style variable
+  var connectColor = "#BFBFBF";
+  var connectWidth = 2;
+  var connectRadius = 8;
+
+  var marqueeBkColro = "rgba(114, 98, 253, 0.1)";
+
+  theme.register("normal", {
+    background: "#fff",
+
+    "root-color": "#fff",
+    "root-background": "#7262FD",
+    // "root-stroke": "red", // 节点边框颜色
+    "root-font-size": 24,
+    "root-padding": [12, 16],
+    "root-margin": 30,
+    "root-radius": 4,
+    "root-space": 10,
+    // "root-shadow": "rgba(0, 0, 0, .25)",
+
+    "main-color": commonFontColor,
+    "main-background": commonBkColor,
+    "main-stroke": "white",
+    "main-font-size": commonFontSize,
+    "main-padding": [5, 12],
+    "main-margin": [20, 20],
+    "main-radius": 6,
+    "main-space": 5,
+    // "main-shadow": "rgba(0, 0, 0, .25)",
+
+    "sub-color": commonFontColor,
+    "sub-background": commonBkColor,
+    "sub-stroke": "white",
+    "sub-font-size": commonFontSize,
+    "sub-padding": [5, 12],
+    "sub-margin": [10, 10],
+    "sub-radius": 5,
+    "sub-space": 5,
+
+    "connect-color": connectColor,
+    "connect-width": connectWidth,
+    "main-connect-width": connectWidth,
+    "connect-radius": connectRadius,
+
+    // "selected-background": "rgb(254, 219, 0)",
+    "selected-stroke": commonColor, // 选中节点边框颜色
+    "selected-stroke-width": 2, // 选中节点边框宽度
+
+    "marquee-background": marqueeBkColro,
+    // "marquee-stroke": "white",
+
+    "drop-hint-color": commonColor,
+    "drop-hint-width": 2,
+
+    "order-hint-area-color": "#E1DFFF",
+    "order-hint-path-color": commonColor,
+    "order-hint-path-width": 0,
+
+    "text-selection-color": "#E1DFFF",
+    "line-height": 1.5,
+  });
+});
